b538425033e09422fc0c4b6d360c0f461c8ef7b4
Jairo García González <57964503+JairoGarGon@users.noreply.github.com>
2019-12-16 19:01:24 +0100

    prueba web JAIRO

---
+++ nueva web jairo.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+$(document).ready(function(){/*Cuando el documento esta listo carga la funcion*/
+  $(window).scroll(function(){ /*Cuando hacemos scroll carga la funcion*/
+  if($(this).scrollTop()>0){
+    $('header').addClass('header2');
+  }else{
+    $('header').removeClass('header2');
+  }
+  });
+});
